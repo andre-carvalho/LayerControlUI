dfa2188639b40be8a2c68ef53f1fcec79b413dcc
Jether Rodrigues <jether.rodrigues@geovoxel.com>
2018-06-19 16:25:02 -0300

    some updates: accordeon, choise baselayers, on-off overlayes, legend area

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {MatExpansionModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatIconModule,
-        MatToolbarModule,
-        MatSliderModule,
-        MatButtonModule} from '@angular/material';
+import { MatExpansionModule,
+          MatFormFieldModule,
+          MatInputModule,
+          MatIconModule,
+          MatToolbarModule,
+          MatSliderModule,
+          MatButtonModule,
+          MatRadioModule,
+          MatSlideToggleModule,
+          MatCardModule } from '@angular/material';
 
 import { LayerToolsComponent } from './layer-tools/layer-tools.component';
 import { LayerGroupComponent } from './layer-group/layer-group.component';
@@ -33,7 +36,14 @@ import 'hammerjs';
     MatIconModule,
     MatToolbarModule,
     MatSliderModule,
-    MatButtonModule
+    MatButtonModule,
+    MatRadioModule,
+    MatSlideToggleModule,
+    MatCardModule
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA, 
+    NO_ERRORS_SCHEMA
   ],
   providers: [],
   bootstrap: [AppComponent]
